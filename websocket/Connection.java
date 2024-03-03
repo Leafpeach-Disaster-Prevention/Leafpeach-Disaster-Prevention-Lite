cf4c149702d5fbdbb9d1a4e9d8c1d9792e8be1ff
dxrxie2019 <2194362576@qq.com>
2024-03-03 10:25:34 +0800

    v2.0.6
    fix bugs

---
+++ websocket/Connection.java
@@ -0,0 +1,40 @@
+package cn.leafpeach.liteapp.app.websocket;
+
+import org.java_websocket.WebSocket;
+
+import java.net.URI;
+
+public class Connection {
+    public static WSClient client;
+    public static LPClient lpClient;
+
+    public static void connect() {
+        try {
+            client = new WSClient(new URI("wss://ws-api.wolfx.jp/all_eew"));
+            client.connect();
+            while (!client.getReadyState().equals(WebSocket.READYSTATE.OPEN)) {
+                Thread.sleep(1000);
+            }
+            while (WSClient.getExcptMessage() == null) {
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void IclConnect() {
+        try {
+            lpClient = new LPClient(new URI("ws://49.232.181.118:49133"));
+            lpClient.connect();
+            while (!lpClient.getReadyState().equals(WebSocket.READYSTATE.OPEN)) {
+                Thread.sleep(1000);
+            }
+            while (LPClient.getExcptMessage() == null) {
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
